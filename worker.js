1fed3c71d1d98d434e797c96cfb15b28277fd37b
sohel160 <sohelcmc53rd@gmail.com>
2026-05-01 12:38:27 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -35,11 +35,11 @@ proxies:
   - {name: proxy2, type: http, server: 144.48.108.122, port: 5452}
   - {name: proxy3, type: http, server: 203.76.108.222, port: 27271}
   - {name: proxy4, type: http, server: 103.167.17.220, port: 2610}
-  - {name: proxy5, type: http, server: 27.147.195.166, port: 27271}
+  - {name: proxy5, type: http, server: 203.76.108.222, port: 27271}
   - {name: proxy6, type: http, server: 203.76.112.42, port: 27271}
   - {name: proxy7, type: http, server: 203.76.115.98, port: 27271}
   - {name: proxy8, type: http, server: 203.76.123.234, port: 27271}
-  - {name: proxy9, type: http, server: 27.147.195.166, port: 27271}
+  - {name: proxy9, type: http, server: 27.147.221.155, port: 27271}
   - {name: proxy10, type: http, server: 203.76.126.162, port: 27271}
 `
 
