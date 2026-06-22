e269f1e88e55f969d61a73c51b0ba9042757d281
sohel160 <sohelcmc53rd@gmail.com>
2026-06-22 09:42:24 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -32,54 +32,7 @@ export default {
       const proxies = `
 proxies:
 
-  - name: proxy1
-    type: http
-    server: 103.115.242.240
-    port: 2610
-
-  - name: proxy2
-    type: http
-    server: 103.172.15.9
-    port: 5452
-
-  - name: proxy3
-    type: http
-    server: 144.48.108.121
-    port: 5452
-
-  - name: proxy4
-    type: http
-    server: 144.48.108.122
-    port: 5452
-    
-  - name: proxy5
-    type: http
-    server: 103.172.14.1
-    port: 5452
-
-  - name: proxy6
-    type: http
-    server: 103.172.15.5
-    port: 5452
-
-  - name: proxy7
-    type: http
-    server: 103.172.15.17
-    port: 5452
-    
-  - name: proxy8
-    type: http
-    server: 103.69.151.244
-    port: 2610
-    
-  - name: proxy9
-    type: http
-    server: 103.172.14.253
-    port: 5452
-
-  - name: proxy10
-    type: http
-    server: 103.172.14.245
+  4.245
     port: 5452
 
     
